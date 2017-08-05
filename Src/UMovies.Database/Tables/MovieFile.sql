7c1edc74f61ece5d4ed410b58a1064018155a40f
Mario Moreno <mario.moreno@live.com>
2017-08-05 17:52:02 -0300

    Update database project

--- Src/UMovies.Database/Tables/MovieFile.sql
+++ Src/UMovies.Database/Tables/MovieFile.sql
@@ -1,13 +1,13 @@
-﻿CREATE TABLE [dbo].[MovieFiles] (
+﻿CREATE TABLE [dbo].[MovieFile] (
     [Id]       INT            IDENTITY (1, 1) NOT NULL,
-    [FileName] NVARCHAR (MAX) NULL,
+    [FileName] NVARCHAR (255) NOT NULL,
     [Movie_Id] INT            NOT NULL,
-    CONSTRAINT [PK_dbo.MovieFiles] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [PK_dbo.MovieFile] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_dbo.MovieFiles_dbo.Movie_Movie_Id] FOREIGN KEY ([Movie_Id]) REFERENCES [dbo].[Movie] ([Id]) ON DELETE CASCADE
 );
 
 
 GO
 CREATE NONCLUSTERED INDEX [IX_Movie_Id]
-    ON [dbo].[MovieFiles]([Movie_Id] ASC);
+    ON [dbo].[MovieFile]([Movie_Id] ASC);
 
